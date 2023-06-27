22e7ac830862718436bf2aba1cbd139054e8b113
Neelesh yadav <mktg.eei@gmai.com>
2023-06-27 17:20:22 +0530

    Edit runApi

--- runApis.js
+++ runApis.js
@@ -3,71 +3,71 @@ const axios = require("axios");
 setInterval(async () => {
   try {
     await axios({
-      url: `http://localhost:8080/api/v1/update/industry_status`,
+      url: `http://localhost:8000/api/v1/update/industry_status`,
     });
   } catch (error) {}
 
-  try {
-    await axios({
-      url: `http://localhost:8080/api/v1/send_sms/piyush/inactive_50`,
-    });
-  } catch (error) {}
+  // try {
+  //   await axios({
+  //     url: `http://localhost:8080/api/v1/send_sms/piyush/inactive_50`,
+  //   });
+  // } catch (error) {}
 }, 1000 * 60 * 2);
 
 setInterval(async () => {
+  // try {
+  //   await axios({
+  //     url: `http://localhost:8080/api/v1/send_data/send_to_bihar`,
+  //   });
+  // } catch (error) {}
+
+  // try {
+  //   await axios({
+  //     url: `http://localhost:8080/api/v1/send_data/send_to_cpcb`,
+  //   });
+  // } catch (error) {}
+
+  // try {
+  //   await axios({
+  //     url: `http://localhost:8080/api/v1/send_data/send_to_haryana`,
+  //   });
+  // } catch (error) {}
+
+  // try {
+  //   await axios({
+  //     url: `http://localhost:8080/api/v1/send_data/send_to_jharkhand`,
+  //   });
+  // } catch (error) {}
+
+  // try {
+  //   await axios({
+  //     url: `http://localhost:8080/api/v1/send_data/send_to_punjab`,
+  //   });
+  // } catch (error) {}
+
+  // try {
+  //   await axios({
+  //     url: `http://localhost:8080/api/v1/send_data/send_to_rajasthan`,
+  //   });
+  // } catch (error) {}
+
   try {
     await axios({
-      url: `http://localhost:8080/api/v1/send_data/send_to_bihar`,
+      url: `http://localhost:8000/api/v1/update/spcb_status`,
     });
   } catch (error) {}
 
   try {
     await axios({
-      url: `http://localhost:8080/api/v1/send_data/send_to_cpcb`,
-    });
-  } catch (error) {}
-
-  try {
-    await axios({
-      url: `http://localhost:8080/api/v1/send_data/send_to_haryana`,
-    });
-  } catch (error) {}
-
-  try {
-    await axios({
-      url: `http://localhost:8080/api/v1/send_data/send_to_jharkhand`,
-    });
-  } catch (error) {}
-
-  try {
-    await axios({
-      url: `http://localhost:8080/api/v1/send_data/send_to_punjab`,
-    });
-  } catch (error) {}
-
-  try {
-    await axios({
-      url: `http://localhost:8080/api/v1/send_data/send_to_rajasthan`,
-    });
-  } catch (error) {}
-
-  try {
-    await axios({
-      url: `http://localhost:8080/api/v1/update/spcb_status`,
-    });
-  } catch (error) {}
-
-  try {
-    await axios({
-      url: `http://localhost:8080/api/v1/update/cpcb_status`,
+      url: `http://localhost:8000/api/v1/update/cpcb_status`,
     });
   } catch (error) {}
 }, 1000 * 60 * 5 + 50);
 
-setInterval(async () => {
-  try {
-    await axios({
-      url: `http://localhost:8080/api/v1/send_data/send_to_delhi`,
-    });
-  } catch (error) {}
-}, 1000 * 60 * 16);
+// setInterval(async () => {
+//   try {
+//     await axios({
+//       url: `http://localhost:8080/api/v1/send_data/send_to_delhi`,
+//     });
+//   } catch (error) {}
+// }, 1000 * 60 * 16);
