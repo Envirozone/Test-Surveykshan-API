eaa3f95774a3f5246dfd7c868b9b0ef7dc3f8f74
Neelesh yadav <mktg.eei@gmai.com>
2023-07-05 16:12:56 +0530

    Changed runApis test-api.accesssurveykshan.co.in

--- runApis.js
+++ runApis.js
@@ -9,7 +9,7 @@ setInterval(async () => {
 
   try {
     await axios({
-      url: `http://localhost:8000/api/v1/send_sms/piyush/inactive_50`,
+      url: `http://test-api.accesssurveykshan.co.in/api/v1/send_sms/piyush/inactive_50`,
     });
   } catch (error) {}
 }, 1000 * 60 * 2);
@@ -17,13 +17,13 @@ setInterval(async () => {
 setInterval(async () => {
   try {
     await axios({
-      url: `http://localhost:8000/api/v1/update/spcb_status`,
+      url: `http://test-api.accesssurveykshan.co.in/api/v1/update/spcb_status`,
     });
   } catch (error) {}
 
   try {
     await axios({
-      url: `http://localhost:8000/api/v1/update/cpcb_status`,
+      url: `http://test-api.accesssurveykshan.co.in/api/v1/update/cpcb_status`,
     });
   } catch (error) {}
 }, 1000 * 60 * 5 + 50);
