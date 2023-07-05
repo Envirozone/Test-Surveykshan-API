670475d5cac3ef900e64bfd1bff591717243b184
Neelesh yadav <mktg.eei@gmai.com>
2023-07-05 14:47:56 +0530

    Changed runApis port to 8000

--- runApis.js
+++ runApis.js
@@ -3,13 +3,13 @@ const axios = require("axios");
 setInterval(async () => {
   try {
     await axios({
-      url: `http://localhost:8080/api/v1/update/industry_status`,
+      url: `http://localhost:8000/api/v1/update/industry_status`,
     });
   } catch (error) {}
 
   try {
     await axios({
-      url: `http://localhost:8080/api/v1/send_sms/piyush/inactive_50`,
+      url: `http://localhost:8000/api/v1/send_sms/piyush/inactive_50`,
     });
   } catch (error) {}
 }, 1000 * 60 * 2);
@@ -17,13 +17,13 @@ setInterval(async () => {
 setInterval(async () => {
   try {
     await axios({
-      url: `http://localhost:8080/api/v1/update/spcb_status`,
+      url: `http://localhost:8000/api/v1/update/spcb_status`,
     });
   } catch (error) {}
 
   try {
     await axios({
-      url: `http://localhost:8080/api/v1/update/cpcb_status`,
+      url: `http://localhost:8000/api/v1/update/cpcb_status`,
     });
   } catch (error) {}
 }, 1000 * 60 * 5 + 50);
